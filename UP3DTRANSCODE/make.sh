b12351ca41ac7cf93ef9b2274fdfbd0158cb0ef0
Kai Scheffer <kai@diescheffers.de>
2016-10-06 19:40:43 +0200

    relocated up3dconf.c and .h to UP3DCOMMON

--- UP3DTRANSCODE/make.sh
+++ UP3DTRANSCODE/make.sh
@@ -14,7 +14,7 @@ if [[ "$OSTYPE" == "msys" ]]; then
 
 $CC -std=c99 -Ofast -fwhole-program -flto \
     -I../UP3DCOMMON \
-    -o up3dtranscode.exe up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
+    -o up3dtranscode.exe ../UP3DCOMMON/up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
 
 $STRIP up3dtranscode.exe
 
@@ -26,7 +26,7 @@ $CC -std=c99 -Ofast \
     -framework IOKit \
     -framework CoreFoundation \
     -lobjc \
-    -o up3dtranscode up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
+    -o up3dtranscode ../UP3DCOMMON/up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
 
 $STRIP up3dtranscode
 
@@ -35,7 +35,7 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 $CC -std=c99 -Ofast -fwhole-program -flto \
     -I../UP3DCOMMON \
-    -o up3dtranscode up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
+    -o up3dtranscode ../UP3DCOMMON/up3dconf.c hoststepper.c hostplanner.c gcodeparser.c ../UP3DCOMMON/up3ddata.c umcwriter.c up3dtranscode.c -lm
 
 $STRIP up3dtranscode
 
